7b2b8ed1b913b22850f36af74be27a5a16da6dd1
Forrest Oliphant <forrest@sembiki.com>
2013-01-19 16:39:40 +0100

    tuna nodes show their inputs

--- src/dataflow-webaudio.js
+++ src/dataflow-webaudio.js
@@ -2,6 +2,8 @@
 
 $(function($) {
 
+  // Setup context
+
   var context;
   if (typeof AudioContext !== "undefined") {
     context = new AudioContext();
@@ -11,6 +13,22 @@ $(function($) {
   } else {
     throw new Error('AudioContext not supported. :(');
   }
+
+  // Make base audio modules
+
+  var Base = Dataflow.node("base");
+  var TunaBase = Dataflow.node("tuna-base");
+
+  TunaBase.Model = Base.Model.extend({
+  });
+  TunaBase.View = Base.View.extend({
+    initialize: function(){
+      Base.View.prototype.initialize.call(this);
+      this.$(".inner").text("audio node!");
+    }
+  });
+
+  // Make Tuna effect modules
 
   var tuna = new Tuna(context);
 
@@ -27,11 +45,41 @@ $(function($) {
     Chorus: {}
   };
   for (var mod in tunaModules) {
+    var name = "tuna-"+mod.toLowerCase();
+    var node = tunaModules[mod].node = Dataflow.node(name);
+    var extendwith = {
+      defaults: {
+        label: "",
+        type: name,
+        x: 200,
+        y: 100,
+        w: 200,
+        h: 200
+      },
+      inputs:[
+        {
+          id: "in",
+          type: "audio"
+        }
+      ],
+      outputs:[
+        {
+          id: "out",
+          type: "audio"
+        }
+      ]
+    };
     for (var prop in tuna[mod].prototype) {
-      console.log(mod, prop, tuna[mod].prototype.defaults[prop]);
+      var input = {};
+      input.id = prop;
+      input.type = tuna[mod].prototype.defaults[prop].type;
+      extendwith.inputs.push(input);
     }
+    var Model = tunaModules[mod].node.Model = TunaBase.Model.extend(extendwith);
+    var View = tunaModules[mod].node.View = TunaBase.View.extend({});
   }
 
-  Dataflow.plugins.library.update({exclude:["test", "base", "base-resizable"]});
+  // Refresh library
+  Dataflow.plugins.library.update({exclude:["test", "base", "base-resizable", "tuna-base"]});
 
 });
