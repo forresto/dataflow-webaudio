bdff8efb734abc7cfc056d6e435652a616bfb7be
Forrest Oliphant <forrest@sembiki.com>
2013-01-20 09:19:31 +0100

    tuna nodes input works, automatable

--- src/dataflow-webaudio.js
+++ src/dataflow-webaudio.js
@@ -317,6 +317,20 @@ $(function($) {
 
       var tunaNode = this.audioOutput = new tuna[this.tunaName](this.get('state'));
       var audioInput = this.audioInput = tunaNode.input;
+    },
+    setState: function(name, value){
+      AudioBase.Model.prototype.setState.call(this, name, value);
+      // var state = this.get("state");
+      // state[name] = value;
+      // if (this["input"+name]){
+      //   this["input"+name](value);
+      // }
+
+      if (!this["input"+name]) {
+        if (this.audioOutput[name] !== undefined) {
+          this.audioOutput[name].value = value;
+        }
+      }
     }
   };
 
@@ -402,7 +416,7 @@ $(function($) {
         edge.source.parentNode.audioOutput.connect(edge.target.parentNode.audioInput);
       }
     } else if (edge.source.get("type") === "audio" && edge.target.get("automatable") === true) {
-      // Make AudioParam and connect it
+      // Connect audio to automatable AudioParam
       if (edge.source.parentNode.audioOutput && edge.target.parentNode.audioOutput) {
         edge.source.parentNode.audioOutput.connect(edge.target.parentNode.audioOutput[edge.target.id]);
       }
@@ -416,7 +430,7 @@ $(function($) {
         edge.source.parentNode.audioOutput.disconnect(edge.target.parentNode.audioInput);
       }
     } else if (edge.source.get("type") === "audio" && edge.target.get("automatable") === true) {
-      // Make AudioParam and connect it
+      // Connect audio to automatable AudioParam
       if (edge.source.parentNode.audioOutput && edge.target.parentNode.audioOutput) {
         edge.source.parentNode.audioOutput.disconnect(edge.target.parentNode.audioOutput[edge.target.id]);
       }
@@ -425,7 +439,7 @@ $(function($) {
 
 
   // Load test graph
-  var testGraph = {"nodes":[{"id":1,"label":"LFO osc","type":"audio-oscillator","x":270,"y":36,"state":{"type":0,"frequency":10,"detune":0}},{"id":4,"label":"audio-oscillator","type":"audio-oscillator","x":440,"y":303,"state":{"type":0,"detune":0,"frequency":300}},{"id":3,"label":"LFO gain","type":"audio-gain","x":533,"y":46,"state":{"gain":50}},{"id":5,"label":"audio-gain","type":"audio-gain","x":699,"y":358,"state":{"gain":1}},{"id":2,"label":"audio-destination","type":"audio-destination","x":961,"y":383,"state":{}}],"edges":[{"source":{"node":5,"port":"out"},"target":{"node":2,"port":"in"}},{"source":{"node":1,"port":"out"},"target":{"node":3,"port":"in"}},{"source":{"node":3,"port":"out"},"target":{"node":4,"port":"frequency"}},{"source":{"node":4,"port":"out"},"target":{"node":5,"port":"in"}}]};
+  var testGraph = {"nodes":[{"id":1,"label":"LFO osc","type":"audio-oscillator","x":287,"y":39,"state":{"type":2,"frequency":-0.5,"detune":0}},{"id":5,"label":"audio-gain","type":"audio-gain","x":300,"y":292,"state":{"gain":1}},{"id":7,"label":"audio-oscillator","type":"audio-oscillator","x":312,"y":449,"state":{"type":3,"frequency":10,"detune":0}},{"id":3,"label":"LFO gain","type":"audio-gain","x":533,"y":46,"state":{"gain":100}},{"id":6,"label":"tuna-overdrive","type":"tuna-overdrive","x":591,"y":373,"state":{"drive":1,"outputGain":0.5,"curveAmount":0.725,"algorithmIndex":0}},{"id":4,"label":"audio-oscillator","type":"audio-oscillator","x":765,"y":98,"state":{"type":0,"detune":0,"frequency":200}},{"id":2,"label":"audio-destination","type":"audio-destination","x":861,"y":400,"state":{}}],"edges":[{"source":{"node":1,"port":"out"},"target":{"node":3,"port":"in"}},{"source":{"node":3,"port":"out"},"target":{"node":4,"port":"frequency"}},{"source":{"node":4,"port":"out"},"target":{"node":5,"port":"in"}},{"source":{"node":5,"port":"out"},"target":{"node":6,"port":"in"}},{"source":{"node":6,"port":"out"},"target":{"node":2,"port":"in"}},{"source":{"node":7,"port":"out"},"target":{"node":6,"port":"drive"}}]};
   // All nodes
   // var testGraph = {"nodes":[{"id":10,"label":"tuna-compressor","type":"tuna-compressor","x":352,"y":461,"state":{"threshold":-20,"release":250,"makeupGain":1,"attack":1,"ratio":4,"knee":5,"automakeup":false,"bypass":true}},{"id":4,"label":"audio-oscillator","type":"audio-oscillator","x":353,"y":225,"state":{}},{"id":1,"label":"audio-buffersource","type":"audio-buffersource","x":355,"y":20,"state":{}},{"id":13,"label":"tuna-cabinet","type":"tuna-cabinet","x":579,"y":496,"state":{"makeupGain":1,"bypass":false}},{"id":8,"label":"tuna-tremolo","type":"tuna-tremolo","x":579,"y":25,"state":{"intensity":0.3,"stereoPhase":0,"rate":5}},{"id":15,"label":"tuna-overdrive","type":"tuna-overdrive","x":580,"y":672,"state":{"drive":1,"outputGain":1,"curveAmount":0.725,"algorithmIndex":0}},{"id":6,"label":"tuna-chorus","type":"tuna-chorus","x":580,"y":236,"state":{"feedback":0.4,"delay":0.0045,"depth":0.7,"rate":1.5,"bypass":true}},{"id":7,"label":"tuna-phaser","type":"tuna-phaser","x":807,"y":24,"state":{"rate":0.1,"depth":0.6,"feedback":0.7,"stereoPhase":40,"baseModulationFrequency":700}},{"id":11,"label":"tuna-convolver","type":"tuna-convolver","x":807,"y":561,"state":{"highCut":22050,"lowCut":20,"dryLevel":1,"wetLevel":1,"level":1}},{"id":3,"label":"tuna-delay","type":"tuna-delay","x":808,"y":306,"state":{"delayTime":100,"feedback":0.45,"cutoff":20000,"wetLevel":0.5,"dryLevel":1}},{"id":9,"label":"tuna-wahwah","type":"tuna-wahwah","x":1034,"y":20,"state":{"automode":true,"baseFrequency":0.5,"excursionOctaves":2,"sweep":0.2,"resonance":10,"sensitivity":0.5}},{"id":2,"label":"audio-destination","type":"audio-destination","x":1038,"y":731,"state":{}},{"id":14,"label":"tuna-filter","type":"tuna-filter","x":1035,"y":307,"state":{"frequency":800,"Q":1,"gain":0,"bypass":true,"filterType":1}},{"id":5,"label":"audio-gain","type":"audio-gain","x":1036,"y":565,"state":{}}],"edges":[]};
   var g = Dataflow.loadGraph(testGraph);
